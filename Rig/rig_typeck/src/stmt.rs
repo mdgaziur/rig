4380d787a36411ea67b7ba46d8d22557e36080da
mdgaziur <mdgaziurrahmannoor@gmail.com>
2022-10-03 20:13:01 +0600

    Typechecker: basic function and body typechecking

--- Rig/rig_typeck/src/stmt.rs
+++ Rig/rig_typeck/src/stmt.rs
@@ -8,11 +8,13 @@ use rig_project::Project;
 use rig_session::Session;
 
 use rig_ast::expr::Expr;
+use rig_ast::function_prototype::Prototype;
 use rig_ast::struct_field::StructField;
 use rig_ast::visibility::Visibility;
 use rig_span::Span;
-use rig_types::checked_stmt::CheckedStmt;
-use rig_types::{ModuleId, ScopeId, StructFieldType, StructType, Type, TypeId, TypeIdOrModuleId};
+use rig_types::builtins::UNDEFINED_TYPEID;
+use rig_types::checked_stmt::{CheckedBlockStmt, CheckedStmt};
+use rig_types::{FunctionArgument, FunctionType, ModuleId, ScopeId, StructFieldType, StructType, Type, TypeId, TypeIdOrModuleId};
 use rig_utils::bug;
 use std::collections::HashMap;
 
@@ -54,6 +56,29 @@ pub fn typecheck_statement(
             name,
             fields,
             span,
+        ),
+        Stmt::FnStmt {
+            visibility,
+            prototype,
+            body,
+            span,
+        } => check_fn(
+            project,
+            session,
+            typechecker_errors,
+            module_id,
+            scope_id,
+            *visibility,
+            prototype,
+            body,
+            span,
+        ),
+        Stmt::BlockStmt { .. } => check_body(
+            session,
+            stmt,
+            module_id,
+            project,
+            typechecker_errors,
         ),
         _ => todo!(),
     }
@@ -211,4 +236,263 @@ fn check_struct(
     );
 
     (None, errors)
+}
+
+fn check_fn(
+    project: &mut Project,
+    session: &Session,
+    typechecker_errors: &mut HashMap<ModuleId, Vec<RigError>>,
+    module_id: ModuleId,
+    scope_id: ScopeId,
+    visibility: Visibility,
+    prototype: &Prototype,
+    body: &Box<Stmt>,
+    span: &Span,
+) -> (Option<CheckedStmt>, Vec<(ModuleId, RigError)>) {
+    let mut errors = vec![];
+
+    // first check if it's already declared in the module
+    let module = project.get_module(module_id);
+    if let Some((_, function_id)) = module.get_scope(scope_id).find_fn(&prototype.name) {
+        // do not allow multiple definition of struct with same name in same module
+        if function_id.get_module_id() == module_id {
+            let resolved_struct = project.resovle_fn(function_id);
+
+            errors.push((
+                module_id,
+                RigError::with_hint(
+                    ErrorType::Hard,
+                    ErrorCode::E0008,
+                    "Redefinition of function",
+                    span.clone(),
+                    "The other one is defined here",
+                    resolved_struct.span.clone(),
+                ),
+            ));
+        }
+    }
+
+    let mut typechecked_arguments = vec![];
+
+    for arg in &prototype.args {
+        let name = arg.name.clone();
+        let ty = arg.type_.clone();
+        let ty_path;
+        let ty_path_span;
+
+        match ty {
+            Expr::PathExpr { path, span } => {
+                ty_path = path;
+                ty_path_span = span;
+            }
+            Expr::VariableExpr { name, span } => {
+                ty_path = vec![name];
+                ty_path_span = span;
+            }
+            expr => bug!(expr, "Unexpected expression in type of struct field"),
+        }
+
+        let ty = match module.try_resolve(&project.modules, &ty_path) {
+            Ok(ty) => ty,
+            Err(error) => {
+                errors.push((
+                    module_id,
+                    RigError::with_no_hint_and_notes(
+                        ErrorType::Hard,
+                        error.to_error_code(),
+                        &error.to_string(),
+                        ty_path_span.clone(),
+                    ),
+                ));
+
+                return (None, errors);
+            }
+        };
+
+        let ty = match ty {
+            TypeIdOrModuleId::TypeId(ty, vis) => {
+                if ty.get_module_id() != module_id && vis != Visibility::Pub {
+                    errors.push((
+                        module_id,
+                        RigError::with_no_hint_and_notes(
+                            ErrorType::Hard,
+                            ErrorCode::E0010,
+                            "Attempt to import private type",
+                            ty_path_span.clone(),
+                        ),
+                    ));
+
+                    continue;
+                } else {
+                    ty
+                }
+            }
+            TypeIdOrModuleId::ModuleId(_, _) => {
+                errors.push((
+                    module_id,
+                    RigError::with_no_hint_and_notes(
+                        ErrorType::Hard,
+                        ErrorCode::E0014,
+                        "Expected type, found module",
+                        ty_path_span.clone(),
+                    ),
+                ));
+                continue;
+            }
+        };
+
+        typechecked_arguments.push(FunctionArgument {
+            name,
+            ty,
+            span: arg.span.clone(),
+        })
+    }
+
+    let return_type;
+    if let Some(return_ty) = prototype.return_ty.clone() {
+        let ty_path;
+        let ty_path_span;
+
+        match return_ty {
+            Expr::PathExpr { path, span } => {
+                ty_path = path;
+                ty_path_span = span;
+            }
+            Expr::VariableExpr { name, span } => {
+                ty_path = vec![name];
+                ty_path_span = span;
+            }
+            expr => bug!(expr, "Unexpected expression in type of struct field"),
+        }
+
+        let ty = match module.try_resolve(&project.modules, &ty_path) {
+            Ok(ty) => ty,
+            Err(error) => {
+                errors.push((
+                    module_id,
+                    RigError::with_no_hint_and_notes(
+                        ErrorType::Hard,
+                        error.to_error_code(),
+                        &error.to_string(),
+                        ty_path_span.clone(),
+                    ),
+                ));
+
+                return (None, errors);
+            }
+        };
+
+        return_type = match ty {
+            TypeIdOrModuleId::TypeId(ty, vis) => {
+                if ty.get_module_id() != module_id && vis != Visibility::Pub {
+                    errors.push((
+                        module_id,
+                        RigError::with_no_hint_and_notes(
+                            ErrorType::Hard,
+                            ErrorCode::E0010,
+                            "Attempt to import private type",
+                            ty_path_span.clone(),
+                        ),
+                    ));
+
+                    return (None, errors);
+                } else {
+                    ty
+                }
+            }
+            TypeIdOrModuleId::ModuleId(_, _) => {
+                errors.push((
+                    module_id,
+                    RigError::with_no_hint_and_notes(
+                        ErrorType::Hard,
+                        ErrorCode::E0014,
+                        "Expected type, found module",
+                        ty_path_span.clone(),
+                    ),
+                ));
+
+                return (None, errors);
+            }
+        };
+    } else {
+        return_type = UNDEFINED_TYPEID;
+    }
+
+    let (_, body_errors) = check_body(
+        session,
+        body,
+        module_id,
+        project,
+        typechecker_errors,
+    );
+
+    errors.extend(body_errors);
+
+    let module = project.get_module_mut(module_id);
+    let mut location = module.location.clone();
+    location.push(prototype.name.clone());
+
+    let function_type = FunctionType {
+        return_ty: Some(return_type),
+        args: typechecked_arguments,
+        visibility,
+        span: span.clone(),
+        location,
+    };
+    module.functions.push(function_type);
+    let type_id = TypeId(
+        scope_id,
+        module.functions.len() - 1,
+        visibility == Visibility::Pub,
+        Type::Function,
+    );
+
+    module.get_scope_mut(scope_id).insert_type(
+        &prototype.name,
+        type_id,
+    );
+
+    (None, errors)
+}
+
+fn check_body(
+    session: &Session,
+    body: &Stmt,
+    module_id: ModuleId,
+    project: &mut Project,
+    typechecker_errors: &mut HashMap<ModuleId, Vec<RigError>>,
+) -> (Option<CheckedStmt>, Vec<(ModuleId, RigError)>) {
+    let mut errors = vec![];
+    let mut checked_stmts = vec![];
+    let scope_id = project.get_module_mut(module_id).new_scope();
+
+    let (stmts, span) = match body {
+        Stmt::BlockStmt { exprs, span } => (exprs, span),
+        _ => bug!(body, "check_body() was called with non-block statement"),
+    };
+
+    for stmt in stmts {
+        let (checked_stmt, mut stmt_errors) = typecheck_statement(
+            project,
+            session,
+            typechecker_errors,
+            module_id,
+            scope_id,
+            stmt,
+        );
+        errors.append(&mut stmt_errors);
+        if let Some(checked_stmt) = checked_stmt {
+            checked_stmts.push(checked_stmt);
+        }
+    }
+
+    // TODO: check whether body returns a value if the function is not void
+    let checked_block_statement = CheckedBlockStmt {
+        stmts: checked_stmts,
+        span: span.clone(),
+        scope_id,
+        returns: false,
+    };
+
+    (Some(CheckedStmt::Block(checked_block_statement)), errors)
 }
